5c945e0b467e3ef9b4f4c05e3beffad956f6f92b
nifelix97 <nifelix97@gmail.com>
2023-06-20 09:58:50 +0200

    conference creating

--- conference/urls.py
+++ conference/urls.py
@@ -3,4 +3,9 @@ from conference import views
 
 urlpatterns = [
     path('conferences/', views.conferences_view),
+    path('conferences/create', views.create_conference_view),
+     path('conferences/<id>', views.conference_view),
+    path('conferences/<id>/update', views.update_conference_view),
+    path('conferences/<id>/delete', views.delete_conference_view),
+    path('conferences/<id>/delete-confirm', views.confirm_delete_view)
 ]
--- conference/views.py
+++ conference/views.py
@@ -39,3 +39,29 @@ import datetime
 def conferences_view(request):
     return render(request, 'felix.html', context={"conferences": conferences})
 
+def create_conference_view(request):
+    return render(request, 'create_conferences.html')
+
+def conference_view(request,id):
+    conference = None
+    for conf in conferences:
+        if conf["id"] == int(id):
+            conference = conf
+    return render(request, "conference/conference.html", context={"conference": conference })
+
+def update_conference_view(request,id):
+    conference = None
+    for conf in conferences:
+        if conf["id"] == int(id):
+            conference = conf
+    return render(request, "conference/updateConference.html", context={"conference": conference})
+
+def delete_conference_view(request,id):
+    conference = None
+    for conf in conferences:
+        if conf["id"] == int(id):
+            conference = conf
+    return render(request, "conference/delete_conference.html", context={"conference" : conference})
+
+def confirm_delete_view(request, id):
+    return render(request, "conference/confirm_delete.html")
